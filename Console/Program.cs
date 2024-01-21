4af07455d65a66d5858323b54a922d816717ea34
sebalopez89 <83309691+sebalopez89@users.noreply.github.com>
2024-01-21 00:58:17 -0300

    initial commit

---
+++ Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using WordFinderNamespace;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+try
+{
+    var initialTime = DateTime.UtcNow;
+    Console.WriteLine("Program started");
+    // Read word matrix from file
+    IEnumerable<string> wordMatrix = File.ReadLines("wordMatrix.txt");
+
+    // Initialize WordFinder with the word matrix
+    WordFinder wordFinder = new WordFinder(wordMatrix);
+
+    // Read word stream from file
+    IEnumerable<string> wordstream = File.ReadLines("wordStream.txt");
+
+    // Find and print top 10 most repeated words
+    var topWords = wordFinder.Find(wordstream);
+
+    Console.WriteLine("Top 10 most repeated words:");
+    foreach (var word in topWords)
+    {
+        Console.WriteLine(word);
+    }
+    TimeSpan ts = DateTime.UtcNow - initialTime;
+    //var timeDif = DateTime.UtcNow.Ticks - initialTime;
+    Console.WriteLine($"Time elapsed: {ts.TotalMilliseconds} milisecs");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
---
+++ Console/WordFinder2.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class WordFinder2
+{
+    private readonly char[,] matrix;
+    private Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+    public WordFinder2(IEnumerable<string> matrix)
+    {
+        int matrixSize = matrix.First().Length;
+        this.matrix = new char[matrixSize, matrixSize];
+
+        for (int i = 0; i < matrixSize; i++)
+        {
+            for (int j = 0; j < matrixSize; j++)
+            {
+                this.matrix[i, j] = matrix.ElementAt(i)[j];
+            }
+        }
+    }
+
+    public IEnumerable<string> Find(IEnumerable<string> wordstream)
+    {
+        var wordStreamSet = new HashSet<string>(wordstream);
+
+        Parallel.For(0, matrix.GetLength(0), i =>
+        {
+            string horizontalWord = "";
+            string verticalWord = "";
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                horizontalWord += matrix[i, j];
+                verticalWord += matrix[j, i];
+            }
+            CountWord(horizontalWord, wordStreamSet);
+            CountWord(verticalWord, wordStreamSet);
+        });
+
+        var topWords = wordCount.OrderByDescending(pair => pair.Value)
+                               .Take(10)
+                               .Select(pair => pair.Key);
+
+        return topWords;
+    }
+
+    private void CountWord(string possibleWord, HashSet<string> wordstreamSet)
+    {
+        foreach (var word in wordstreamSet.Where(possibleWord.Contains))
+        {
+            lock (wordCount)
+            {
+                if (!wordCount.ContainsKey(word))
+                {
+                    wordCount[word] = 1;
+                }
+                else
+                {
+                    wordCount[word]++;
+                }
+            }
+        }
+    }
+}
